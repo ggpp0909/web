9b8e9a27d6cae6aa0211f2664595029342fca209
장영남 <ggpp0909@naver.com>
2021-11-01 00:13:11 +0900

    hide secret key

--- django/1031_all_of_crud_prac/crud/settings.py
+++ django/1031_all_of_crud_prac/crud/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-23$u6av*1q$4q#n$g^4uw$q9=g85swdcj(yeu%ft1q0&^s9t!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
