a63751e8f4dd4c885a445662dd45efee6fa9e7b5
SSAFY <ggpp0909@naver.com>
2022-01-07 22:49:28 +0900

    upload

---
+++ JavaScript/nomadcoders/javascript on the browser/app.js
@@ -0,0 +1,20 @@
+// const title = document.getElementById("title");
+
+// console.dir(title)
+//////////////////
+// const title = document.getElementById("title");
+
+// console.log(title)
+// title.innerText = "Got you!"
+
+// console.log(title.id)
+// console.log(title.className)
+
+const hellos = document.getElementsByClassName("hello")
+console.log(hellos)
+
+// const title = document.getElementsByTagName("h1")
+// console.log(title)
+
+const title = document.querySelectorAll(".hello h1")
+console.log(title)
--- JavaScript/nomadcoders/welcome to javascript/momentum/app.js
+++ JavaScript/nomadcoders/welcome to javascript/momentum/app.js
@@ -1,1 +1,33 @@
-alert("hi")
+// const player = {
+//   name: "yeongnam",
+//   points: 2,
+//   fat: true
+// };
+// console.log(player)
+// console.log(player.name)
+
+// functions
+
+// function sayHello(name) {
+//   console.log("Hello! my name is" + name);
+// }
+
+// sayHello("yeongnam")
+// sayHello("james")
+// sayHello("Jhon")
+// sayHello("gimoring")
+
+// function plus(a, b) {
+//   console.log(a + b)
+// }
+
+// const player = {
+//   name: "jang",
+//   sayHello: function() {
+//     console.log("hello")
+//   }
+// }
+// player.sayHello()
+
+const age = prompt("How old are you?")
+console.log(typeof age)
