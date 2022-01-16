60c44063104510a766b93fe58cd6fd562ca593bc
SSAFY <ggpp0909@naver.com>
2022-01-17 02:07:34 +0900

    1.2 websockets in nodeJS

--- nomad_lecture/zoom_clone_coding/src/server.js
+++ nomad_lecture/zoom_clone_coding/src/server.js
@@ -1,8 +1,20 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http"
 
 const app = express();
 
 app.set("view engine", "pug")
-
+app.set("views", __dirname + "/views")
+app.use("/public", express.static(__dirname + "/public"))
+app.get("/", (req, res) => res.render("home"))
+app.get("/*", (req, res) => res.redirect("/"))
 console.log("hello");
-app.listen(3000);
+
+const handleListen = () => console.log('Listening on http://localhost:3000')
+
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
+
+server.listen(3000, handleListen)
