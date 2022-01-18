089fba6eb4c3218836e274e8a06c955a5e7626ba
SSAFY <ggpp0909@naver.com>
2022-01-18 15:59:15 +0900

    6.1 useEffect

--- nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
@@ -1,11 +1,15 @@
 import Button from "./Button";
 import styles from "./App.module.css"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1)
+  console.log("I run all the time")
 
+  useEffect(() => {
+    console.log("call the api")
+  }, [])
   return (
     <div>
       <h1>{counter}</h1>
