b1c47ca1767e9e4d06de40a6c6a0171ab548256b
SSAFY <ggpp0909@naver.com>
2022-01-19 04:21:00 +0900

    7.6 useParams

--- nomad_lecture/React/4. create react app/react-for-beginners/src/components/Movie.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/components/Movie.js
@@ -8,7 +8,7 @@ function Movie({ id, coverImg, title, year, summary, genres }) {
       <img src={coverImg} alt={title} className={styles.movie__img} />
       <div>
         <h2 className={styles.movie__title}>
-          <Link to={`/movie/`}>{title}</Link>
+          <Link to={`/movie/${id}`}>{title}</Link>
         </h2>
         <h3 className={styles.movie__year}>{year}</h3>
         <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
--- nomad_lecture/React/4. create react app/react-for-beginners/src/routes/Home.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/routes/Home.js
@@ -20,7 +20,7 @@ function Home() {
       {loading ? <h1>loading...</h1> : 
       <div>
         {movies.map((movie) => (
-        <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+        <Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
         ))}
       </div>}
     </div>
