1aae18ff485886a05af315d4da956cd9e61206b6
SSAFY <ggpp0909@naver.com>
2022-01-19 04:06:43 +0900

    7.5 React Router

--- nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- nomad_lecture/React/4. create react app/react-for-beginners/src/components/Movie.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/components/Movie.js
@@ -1,22 +1,33 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({mediumCoverImage, title, summary, genres}) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-  <div>
-  <img src={mediumCoverImage} alt={title}></img>
-  <h2>{title}</h2>
-  <p>{summary}</p>
-  <ul>
-    {genres.map((g) => (<li key={g}>{g}</li>))}
-  </ul>
-</div>
-)}
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 Movie.propTypes = {
-  mediumCoverImage: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired
-}
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default Movie;
