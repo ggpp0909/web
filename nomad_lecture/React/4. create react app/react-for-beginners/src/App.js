3fb7e7c953248217bbbbf6447d96e2ab1b83aa20
SSAFY <ggpp0909@naver.com>
2022-01-19 00:21:51 +0900

    7.0 todolist part 1

--- nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
+++ nomad_lecture/React/4. create react app/react-for-beginners/src/App.js
@@ -1,35 +1,27 @@
-import Button from "./Button";
-import styles from "./App.module.css"
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1)
-  const onChange = (event) => setKeyword(event.target.value)
-  console.log("I run all the time")
-
-  useEffect(() => {
-    console.log("i run only once")
-  }, [])
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("i run when 'keyword' changes", keyword)
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
     }
-  }, [keyword])
-  useEffect(() => {  
-    console.log("i run when 'counter' changes", keyword)
-  }, [counter])
-  useEffect(() => {  
-    console.log("i run when 'keyword or counter' changes", keyword)
-  }, [keyword, counter])
-  
+    setTodos((currentArray) => [toDo, ...currentArray])
+    setTodo("");
+  } 
+  console.log(toDos)
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..."></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My Todos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="write your to do..."></input>
+        <button>Add todo</button>
+      </form>
     </div>
+    
   );
 }
 
