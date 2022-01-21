7ff78e94759979a638f9153ed22f350a652b7b7e
SSAFY <ggpp0909@naver.com>
2022-01-21 12:31:33 +0900

    1.4 refactor

--- nomad_lecture/Redux/vanilla-redux/src/index.js
+++ nomad_lecture/Redux/vanilla-redux/src/index.js
@@ -4,13 +4,24 @@ const add = document.querySelector("#add");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("#number");
 
+const ADD = "ADD" // string으로 오타나면 오류 말해주지 않으므로 이렇게 변경 (안전하게)
+const MINUS = "MINUS"
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // if (action.type === "ADD") {
+  //   return count + 1;
+  // } else if (action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -19,6 +30,6 @@ const onChange = () => {
   number.innerText = countStore.getState()
 }
 countStore.subscribe(onChange)
-add.addEventListener("click", () => countStore.dispatch({type:"ADD"}))
-minus.addEventListener("click", () => countStore.dispatch({type:"MINUS"}))
+add.addEventListener("click", () => countStore.dispatch({type:ADD}))
+minus.addEventListener("click", () => countStore.dispatch({type:MINUS}))
 
