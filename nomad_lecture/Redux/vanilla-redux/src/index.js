20c9f402e3080d1ec99afaed18ed8c481cd37bd2
SSAFY <ggpp0909@naver.com>
2022-01-21 11:33:54 +0900

    1.1 Store and Reducer

--- nomad_lecture/Redux/vanilla-redux/src/index.js
+++ nomad_lecture/Redux/vanilla-redux/src/index.js
@@ -1,23 +1,12 @@
+import { createStore } from "redux";
+
 const add = document.querySelector("#add");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("#number");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-}
-const handleminus = () => {
-  count = count - 1;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleminus);
+const countModifier = (state = 0) => {
+  return state
+};
+const countStore = createStore(countModifier)
+console.log(countStore.getState())
 
