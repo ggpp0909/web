5cb4e717ac9cf19c3fa20ccec94aa645816362b6
SSAFY <ggpp0909@naver.com>
2022-01-21 12:19:46 +0900

    1.2 Actions

--- nomad_lecture/Redux/vanilla-redux/src/index.js
+++ nomad_lecture/Redux/vanilla-redux/src/index.js
@@ -4,9 +4,24 @@ const add = document.querySelector("#add");
 const minus = document.querySelector("#minus");
 const number = document.querySelector("#number");
 
-const countModifier = (state = 0) => {
-  return state
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
-const countStore = createStore(countModifier)
-console.log(countStore.getState())
+
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+
+console.log(countStore.getState()); // 4
 
