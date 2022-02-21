2e0f8b9fccdfa1f4597b997f42ba543d8fe1bebe
장영남 <ggpp0909@naver.com>
2022-02-21 22:44:33 +0900

    #2.4 Animations and Pseudo Selectors

--- nomad_lecture/React_masterclass/styled_component/react-masterclass/src/App.js
+++ nomad_lecture/React_masterclass/styled_component/react-masterclass/src/App.js
@@ -1,40 +1,50 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+
+const Wrapper = styled.div`
   display: flex;
+`
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  } 
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  } 
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
 `
 
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
   height: 100px;
-`
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`
-
-const Input = styled.input.attrs({required: true, minLength: 10})`
+  width: 100px;
   background-color: tomato;
-`
-
-const Button = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `
 
 function App() {
   return (
-    <Father>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Input></Input>
-      <Button>Log in</Button>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>ㅎㅎ</span>
+      </Box>
+    </Wrapper>
   )
 }
 
