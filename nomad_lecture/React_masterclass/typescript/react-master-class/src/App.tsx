32d2fae25d202199a105154318101fb1bb5cc715
장영남 <ggpp0909@naver.com>
2022-03-02 04:26:31 +0900

    #3.6 Themes

--- nomad_lecture/React_masterclass/typescript/react-master-class/src/App.tsx
+++ nomad_lecture/React_masterclass/typescript/react-master-class/src/App.tsx
@@ -1,32 +1,22 @@
 import React, { useState } from "react";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState<string>("")
-
-  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value);
-  }
-
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("hello", value);
-    setValue("")
-  }
 
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Login</button>
-      </form>
-    </div>
+    <Container>
+      <H1>
+        protected
+      </H1>
+    </Container>
   );
 }
 
--- nomad_lecture/React_masterclass/typescript/react-master-class/src/index.tsx
+++ nomad_lecture/React_masterclass/typescript/react-master-class/src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
   
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
