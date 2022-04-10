5893b496f0513f8e85d9c51b0e0faf0c97bd61b8
장영남 <ggpp0909@naver.com>
2022-04-11 01:41:00 +0900

    #4.9 React Query part One

--- nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/routes/Coins.tsx
+++ nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "./api";
 
 const Title = styled.h1`
   color: ${(props) => props.theme.accentColor};
@@ -55,7 +57,7 @@ const Img = styled.img`
 
 
 
-interface CoinInterface {
+interface ICoin {
   id: string,
   name: string,
   symbol: string,
@@ -66,28 +68,29 @@ interface CoinInterface {
 }
 
 const Coins = () => {
-  const [coins, setCoins] = useState<CoinInterface[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  useEffect(() => {
-    (async() => {
-      const response = await fetch("https://api.coinpaprika.com/v1/coins")
-      const json = await response.json()
-      setCoins(json.slice(0, 100))
-      setLoading(false);
-    })();
-  }, []);
+  const {isLoading, data} = useQuery<ICoin[]>("allCoins", fetchCoins)
+  // const [coins, setCoins] = useState<CoinInterface[]>([]);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // useEffect(() => {
+  //   (async() => {
+  //     const response = await fetch("https://api.coinpaprika.com/v1/coins")
+  //     const json = await response.json()
+  //     setCoins(json.slice(0, 100))
+  //     setLoading(false);
+  //   })();
+  // }, []);
 
-  console.log(coins)
+  
   return (
     <Container>
       <Header>
         <Title>코인</Title>
       </Header>
-      {loading ? (
+      {isLoading ? (
         <Loader>"Loading..."</Loader>
       ) : (
         <CoinsList>
-          {coins.map((coin) => (
+          {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link to={`/${coin.id}`} state={{name: coin.name}}>
                 <Img
