b5cbbe637ad09732fefc7d4be58d6e1f889b9d04
장영남 <ggpp0909@naver.com>
2022-06-23 00:29:15 +0900

    #4.13 Price Chart part Two

--- nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/routes/Chart.tsx
+++ nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/routes/Chart.tsx
@@ -1,17 +1,82 @@
 import { useQuery } from "react-query";
 import { useOutletContext } from "react-router-dom";
 import { fetchCoinHistory } from "./api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  close: string;
+  high: string;
+  low: string;
+  market_cap: number;
+  open: string;
+  time_close: number;
+  time_open: number;
+  volume: string;
+}
 
 interface IChart {
-  coinId:string;
+  coinId: string;
 }
 
 const Chart = () => {
-  const data = useOutletContext<IChart>()
-  const { isLoading, data:chartdata } = useQuery(["ohlcv", data.coinId], () => fetchCoinHistory(data.coinId))
-  console.log(data)
-  console.log(chartdata)
-  return <h1>Chart</h1>
-}
+  const data = useOutletContext<IChart>();
+  const { isLoading, data: chartdata } = useQuery<IHistorical[]>(
+    ["ohlcv", data.coinId],
+    () => fetchCoinHistory(data.coinId)
+  );
+  console.log(data);
+  console.log(chartdata);
+  return (
+    <div>
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              data: chartdata?.map((price) => Number(price.close)) as number[],
+              name: "price",
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            stroke: {
+              curve: "smooth",
+              width: 3,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Chart;
