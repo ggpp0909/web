3d4ef7c90515ddcd8ddeaf6dd3a0cd7d3fca0363
장영남 <ggpp0909@naver.com>
2022-07-07 09:08:14 +0900

    #5.7 Form Validation

--- nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/ToDoList.tsx
+++ nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/ToDoList.tsx
@@ -1,40 +1,77 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-const ToDoList = () => {
-  const { register, watch } = useForm();
-
-  return (
-    <div>
-      <form>
-        <input {...register("toDo")} placeholder="Write a to do" />
-        <button>ADD</button>
-      </form>
-    </div>
-  );
-};
-
-/* const ToDoList = () => {
-  const [toDo, setTodo] = useState<string>("");
+/* function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = event;
-    setTodo(value);
+    setToDoError("");
+    setToDo(value);
   };
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(toDo);
+    if (toDo.length < 10) {
+      return setToDoError("To do should be longer");
+    }
+    console.log("submit");
   };
-
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-        <button>ADD</button>
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
       </form>
     </div>
   );
-}; */
+} */
+
+function ToDoList() {
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+  return (
+    <div>
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
 
 export default ToDoList;
