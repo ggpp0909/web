dafc23f34acf77040b044666be567fc772111254
장영남 <ggpp0909@naver.com>
2022-07-27 04:05:40 +0900

    코딩애플 2강 데이터바인딩 문법

--- Vue/vuedongsan/src/App.vue
+++ Vue/vuedongsan/src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  gdgd
+  <!-- {{데이터바인딩}} 하는 이유
+  1. HTML에 하드코딩 해놓으면 나중에 변경 어려움
+  2. Vue의 실시간 자동 렌더링을 위해(중요)
+   -->
+  <!-- 속성도 데이터바인딩 가능
+  :속성="데이터이름" -->
+  <div>
+    <h4 class="red" :style="style">{{ products[0] }}</h4>
+    <p> {{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p> {{ price2  }} 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p> {{ price2  }} 만원</p>
+  </div>
+  
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  // 자주 변할거 같은 데이터들은 밑의 데이터 통에다가 보관
+  data () {
+    return {
+      price1 : 60,
+      price2 : 70,
+      style: "color : blue",
+      products : ["역삼동원룸", "천호동원룸", "마포구원룸"]
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
