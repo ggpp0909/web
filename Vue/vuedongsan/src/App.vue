8d0c8160ff687f6cf86624b076e5a32c01558397
장영남 <ggpp0909@naver.com>
2022-07-27 19:59:04 +0900

    Vue 5강 : 모달창 만들기

--- Vue/vuedongsan/src/App.vue
+++ Vue/vuedongsan/src/App.vue
@@ -1,53 +1,84 @@
 <template>
-<!-- 반복문 v-for
-v-for="작명 in 몇회" :kew="작명"
-몇회 자리에는 데이터 집어넣기 가능-->
+  <div v-if="modalState" class="black-bg" @click="modalState = !modalState">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div>
+
   <div class="menu">
-    <a v-for="(item, idx) in menus" :key="idx">{{item}}</a>
+    <a v-for="(item, idx) in menus" :key="idx">{{ item }}</a>
+  </div>
+
+  <div>
+    <img src="./assets/room0.jpg" alt="" class="room-img" />
+    <h4 class="red" :style="style" @click="modalState = !modalState">
+      {{ products[0] }}
+    </h4>
+    <p>{{ price1 }} 만원</p>
+    <button @click="increase(0)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[0] }}</span>
   </div>
   <div>
-    <h4 class="red" :style="style">{{ products[0] }}</h4>
-    <p> {{ price1 }} 만원</p>
-    <button @click="increase">허위매물신고</button> <span>신고수 : {{reportNum}}</span>
-  </div>
-  <div>
+    <img src="./assets/room1.jpg" alt="" class="room-img" />
     <h4>{{ products[1] }}</h4>
-    <p> {{ price2  }} 만원</p>
-    <button></button>
+    <p>{{ price2 }} 만원</p>
+    <button @click="increase(1)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[1] }}</span>
   </div>
   <div>
+    <img src="./assets/room2.jpg" alt="" class="room-img" />
     <h4>{{ products[2] }}</h4>
-    <p> {{ price2  }} 만원</p>
+    <p>{{ price2 }} 만원</p>
+    <button @click="increase(2)">허위매물신고</button>
+    <span>신고수 : {{ reportNum[2] }}</span>
   </div>
-  
 </template>
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
   // 자주 변할거 같은 데이터들은 밑의 데이터 통에다가 보관
-  data () {
+  data() {
     return {
-      reportNum: 0,
-      price1 : 60,
-      price2 : 70,
+      modalState: false,
+      reportNum: [0, 0, 0],
+      price1: 60,
+      price2: 70,
       style: "color : blue",
       menus: ["Home", "Products", "About"],
-      products : ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      prices: [50, 243, 12]
-    }
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      prices: [50, 243, 12],
+    };
   },
   methods: {
-    increase() {
-      this.reportNum += 1
-    }
+    increase(i) {
+      this.reportNum[i] += 1;
+    },
   },
-  components: {
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -66,5 +97,10 @@ export default {
   color: white;
   padding: 10px;
   text-decoration: none;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
