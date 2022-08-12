f068b97459cc8cd56e68373ab96c9c654a076d19
장영남 <ggpp0909@naver.com>
2022-08-13 01:32:40 +0900

    #5.17 Selectors part Two

--- nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/components/ToDoList.tsx
+++ nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/components/ToDoList.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "../atoms";
+import { categoryState, toDoSelector, toDoState } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,33 +8,24 @@ function ToDoList() {
   // const value = useRecoilValue(toDoState) // 리코일에서 값을 불러오는법
   // const modifierFn = useSetRecoilState(toDoState) // 리코일에서 값을 변경하는법, useState의 value와 setValue와 유사
   // const toDos = useRecoilValue(toDoState);
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
-
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (e: React.FocusEvent<HTMLSelectElement>) => {
+    setCategory(e.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onChange={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo}></ToDo>
+      ))}
     </div>
   );
 }
