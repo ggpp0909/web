a6a7b936bb4e4e764cf9fe5471c983ea4798e848
장영남 <ggpp0909@naver.com>
2022-08-15 13:14:53 +0900

    #6.1 Set Selectors

--- nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/App.tsx
+++ nomad_lecture/React_masterclass/crypto tracker/react-master-class/src/App.tsx
@@ -4,10 +4,15 @@ import { hourSelector, minuteState } from "./atoms";
 
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (e: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+e.currentTarget.value);
   };
+
+  const onHoursChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(+e.currentTarget.value);
+  };
+
   return (
     <div>
       <input
@@ -16,7 +21,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
