c4a2f370060bc093e094be44d03c5af119b50ce5
장영남 <ggpp0909@naver.com>
2022-09-22 01:05:39 +0900

    #2.5 Styles

--- React Native/ynWeather/App.js
+++ React Native/ynWeather/App.js
@@ -1,15 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function App() {
   return (
-    // 보모의 flex는 필수
-    <View style={{ flex: 1 }}>
-      {/* 3개의 view 크기를 비율로 조절 가능 */}
-      <View style={{flex: 5, backgroundColor:"tomato"}}></View>
-      <View style={{flex: 3, backgroundColor:"teal"}}></View>
-      <View style={{flex: 1, backgroundColor:"orange"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>12</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, backgroundColor: "tomato"
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 168,
+    marginTop: 50,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60
+  },
+
+})
