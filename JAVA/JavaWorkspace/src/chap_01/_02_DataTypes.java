95aac864c647cb786f94925fd455766f4544e304
장영남 <ggpp0909@naver.com>
2022-12-28 01:11:08 +0900

    chap_02 DataTypes

---
+++ JAVA/JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,19 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        // 문자열
+        System.out.println("Hello World");
+        System.out.println("안녕하세요?");
+        // 숫자
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        // 불리안
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(false);
+        System.out.println(3 + 4);
+    }
+}
+
