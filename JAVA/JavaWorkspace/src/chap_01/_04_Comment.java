b90abfe5c63a0631b02fe01af8dae6a33ac94c7c
장영남 <ggpp0909@naver.com>
2022-12-28 04:58:47 +0900

    chap_04 Comment

---
+++ JAVA/JavaWorkspace/src/chap_01/_04_Comment.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _04_Comment {
+    public static void main(String[] args) {
+//        System.out.println("주석처리");
+        System.out.println("이건 프린트됨");
+/*
+        System.out.println("여러줄 주석");
+        System.out.println("여러줄 주석");
+        System.out.println("여러줄 주석");
+        System.out.println("여러줄 주석");
+ */
+        // 한줄 주석 단축 키 : cmd + /
+        // 여러 줄 주석 단축 키 : cmd + shift + /
+    }
+}
