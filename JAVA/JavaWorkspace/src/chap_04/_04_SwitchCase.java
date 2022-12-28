f61654a00c1ff2a9b766238a4fe5770a3161e5f3
장영남 <ggpp09@gmail.com>
2022-12-28 20:53:01 +0900

    20 Switch Case

---
+++ JAVA/JavaWorkspace/src/chap_04/_04_SwitchCase.java
@@ -0,0 +1,54 @@
+package chap_04;
+
+public class _04_SwitchCase {
+    public static void main(String[] args) {
+        // Switch Case
+
+        // 1등 : 전액 장학금
+        // 2등 : 반액 장학금
+        // 3등 : 반액 장학금
+        // 그외 : 장학금 대상 아님
+
+        int ranking = 1;
+        if (ranking == 1) {
+            System.out.println("전액 장학금");
+        } else if (ranking == 2) {
+            System.out.println("반액 장학금");
+        } else if (ranking == 3) {
+            System.out.println("반액 장학금");
+        } else {
+            System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료");
+
+        // Switch Case 문을 이용
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2:
+                System.out.println("반액 장학금");
+                break;
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 2");
+
+        // Case2와 3을 통합
+        switch (ranking) {
+            case 1:
+                System.out.println("전액 장학금");
+                break;
+            case 2: 
+            case 3:
+                System.out.println("반액 장학금");
+                break;
+            default:
+                System.out.println("장학금 대상 아님");
+        }
+        System.out.println("조회 완료 2");
+    }
+}
