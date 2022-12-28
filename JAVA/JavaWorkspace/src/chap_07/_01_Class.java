528e73595b50080028ebd8aebe4a3565b805ef57
장영남 <ggpp09@gmail.com>
2022-12-29 04:02:41 +0900

    41 Class

---
+++ JAVA/JavaWorkspace/src/chap_07/_01_Class.java
@@ -0,0 +1,30 @@
+package chap_07;
+
+public class _01_Class {
+    public static void main(String[] args) {
+        // 객체 지향 프로그래밍 (OOP : Object-Oriented Programming)
+        // 유지보수 용이
+        // 높은 재사용성
+
+        // 차량용 블랙박스
+
+        // 우리가 만든 첫 번째 제품
+        // 모델명, 해상도, 가격, 색상
+        String modelName = "까망이";
+        String resolution = "FHD";
+        int price = 200000;
+        String color = "블랙";
+
+        // 새로운 제품을 개발
+        String modelName2 = "하양이";
+        String resolution2 = "UHD";
+        int price2 = 300000;
+        String color2 = "화이트";
+
+        // 또다른 제품을 개발? 이럴때 클래스를 쓰면 된다.
+        // BlackBox 클래스로부터 bbox 객체 생성
+        // bbox 객체는 BlackBox 클래스의 인스턴스
+        BlackBox bbox = new BlackBox();
+        BlackBox bbox2 = new BlackBox(); // 여러개 생성도 가능
+    }
+}
