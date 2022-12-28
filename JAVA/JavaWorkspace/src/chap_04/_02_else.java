607d1f618c407d064f2d8f13b2b20190e81780cf
장영남 <ggpp09@gmail.com>
2022-12-28 20:43:49 +0900

    18 else

---
+++ JAVA/JavaWorkspace/src/chap_04/_02_else.java
@@ -0,0 +1,13 @@
+package chap_04;
+
+public class _02_else {
+    public static void main(String[] args) {
+        // 조건문 if else
+        int hour = 10;
+        if (hour < 14) {
+            System.out.println("아메리카노 + 1");
+        } else {
+            System.out.println("아메리카노 디카페인 + 1");
+        }
+    }
+}
