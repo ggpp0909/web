9a18f0128c2ae4a55471f9387608e4b459ab0936
장영남 <ggpp09@gmail.com>
2022-12-28 18:45:08 +0900

    10 operator3

---
+++ JAVA/JavaWorkspace/src/chap_02/_03_Operator3.java
@@ -0,0 +1,18 @@
+package chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        // 비교 연산자
+        System.out.println(5 > 3); // true
+        System.out.println(5 < 3); // false
+        System.out.println(5 >= 3); // true
+        System.out.println(5 <= 3); // false
+        System.out.println(3 <= 3); // true
+
+        System.out.println(5 == 5); // true
+        System.out.println(5 == 3); // false
+        System.out.println(5 != 5); // false
+        System.out.println(5 != 3); // true
+
+    }
+}
