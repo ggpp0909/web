b2fbd98eefaa4a16b3694282b1086f3da847054b
장영남 <ggpp0909@naver.com>
2022-12-28 17:15:37 +0900

    chap_06 Constants

---
+++ JAVA/JavaWorkspace/src/chap_01/_06_Constants.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        // 변수는 선언 후 수정 가능, final 키워드로 선언한 상수는 수정 불가능
+        // 상수는 대문자, 밑줄로 하는게 국룰
+        final String KR_COUNTRY_CODE = "+82"; // 국가 번호
+//        KR_COUNTRY_CODE = "+8282";
+
+        final double PI = 3.141592; // 원주율
+        final String DATE_OF_BIRTH = "1234-56-78"; // 생일
+    }
+}
