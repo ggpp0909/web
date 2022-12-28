1942188a831c8c655bfd3bef246b4d296335b2fa
장영남 <ggpp09@gmail.com>
2022-12-29 04:18:18 +0900

    43 class variables

--- JAVA/JavaWorkspace/src/chap_07/BlackBox.java
+++ JAVA/JavaWorkspace/src/chap_07/BlackBox.java
@@ -4,5 +4,9 @@ public class BlackBox {
     String modelName; // 모델 명
     String resolution; // 해상도
     int price; // 가격
-    String color; // 색상
+    String color; // 색
+
+    // static 이 붙으면 클래스 변수, 위에 안붙은건 인스턴스 변수
+    // 클래스변수는 클래스로부터 만들어지는 모든 객체에 똑같이 적용
+    static boolean canAutoReport = false; // 자동 신고 기능
 }
