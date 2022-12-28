58c191c56bfdb1cfac35c07be89c5aa7057b60ce
장영남 <ggpp09@gmail.com>
2022-12-29 05:09:02 +0900

    45 method overloading

--- JAVA/JavaWorkspace/src/chap_07/BlackBox.java
+++ JAVA/JavaWorkspace/src/chap_07/BlackBox.java
@@ -31,4 +31,20 @@ public class BlackBox {
         }
         return 10;
     }
+
+    void record(boolean showDateTime, boolean showSpeed, int min) {
+        System.out.println("녹화를 시작합니다.");
+        if (showDateTime) {
+            System.out.println("영상에 날짜정보가 표시됩니다.");
+        }
+        if (showSpeed) {
+            System.out.println("영상에 속도정보가 표시됩니다.");
+        }
+        System.out.println("영상은 " + min + "분 단위로 표시됩니다.");
+    }
+
+    void record() {
+        // 유지보수를 위해 일일히 쓰지않고 호출하기
+        record(true, true, 5);
+    }
 }
