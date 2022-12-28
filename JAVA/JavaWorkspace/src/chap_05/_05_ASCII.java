b6beb9a0427c23e29b62563af9d60616d40178d8
장영남 <ggpp09@gmail.com>
2022-12-28 23:57:52 +0900

    32 ASCII

---
+++ JAVA/JavaWorkspace/src/chap_05/_05_ASCII.java
@@ -0,0 +1,17 @@
+package chap_05;
+
+public class _05_ASCII {
+    public static void main(String[] args) {
+        // 아스키코 (ANSI): 미국 표준 코드
+        char c = 'A';
+        System.out.println(c);
+        System.out.println((int) c);
+
+        c = 'B';
+        System.out.println(c);
+        System.out.println((int) c);
+
+        c++;
+        System.out.println(c);
+    }
+}
