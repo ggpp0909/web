2da182bf01a40993bd6525311ac16e6775e8e5bf
장영남 <ggpp09@gmail.com>
2022-12-30 06:31:58 +0900

    67 ArrayList 2

--- JAVA/JavaWorkspace/src/chap_09/_04_ArrayList.java
+++ JAVA/JavaWorkspace/src/chap_09/_04_ArrayList.java
@@ -1,6 +1,7 @@
 package chap_09;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class _04_ArrayList {
     public static void main(String[] args) {
@@ -37,5 +38,32 @@ public class _04_ArrayList {
         for (String s : list) {
             System.out.println(s);
         }
+        System.out.println(" ------ ");
+
+        // 데이터 변경
+        list.get(0);
+        list.set(0, "이수근");
+        list.get(0);
+        System.out.println(" ------ ");
+
+        // 인덱스 확인
+        System.out.println(list.indexOf("김종국"));
+
+        // 리스트 내에 있는 지?
+        System.out.println(list.contains("김종국"));
+
+        // 전체 삭제
+        list.clear();
+        System.out.println(list.isEmpty());
+
+        list.add("유재석");
+        list.add("조세호");
+        list.add("김종국");
+        list.add("박명수");
+        list.add("강호동");
+
+        // 정렬
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
