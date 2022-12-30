0bb6d96555595983c424e46c2bee7da2c8b04eb7
장영남 <ggpp09@gmail.com>
2022-12-30 23:03:48 +0900

    74 lambda

---
+++ JAVA/JavaWorkspace/src/chap_10/_03_Lambda.java
@@ -0,0 +1,16 @@
+package chap_10;
+
+public class _03_Lambda {
+    public static void main(String[] args) {
+        // 람다식
+        // (전달 값1, 전달값2, ...) -> { 코드 }
+
+        // 접근제어자, 이름 필요없음, 리턴타입명시도 필요없음
+        
+    }
+
+//    public void print() {
+//        String s = "test";
+//        System.out.println(s);
+//    }
+}
