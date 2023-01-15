f7340625fc905dd69c9b5938289dc27bec27ee0b
장영남 <ggpp09@gmail.com>
2023-01-15 10:36:28 +0900

    회원 목록 조회

--- spring/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ spring/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -42,5 +43,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> member = memberService.findMember();
+        model.addAttribute("members", member);
+        return "members/memberList";
+
     }
 }
